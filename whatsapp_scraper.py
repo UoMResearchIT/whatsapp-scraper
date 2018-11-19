5b6b47b70c92616dbac7b2a3452fc5623a38ba3e
mfbxkjw7 <joshua.woodcock@manchester.ac.uk>
2018-11-19 11:52:35 +0000

    Added a loop that loops over each text file that isn't the requirements and creates the CSV and Excel files

--- whatsapp_scraper.py
+++ whatsapp_scraper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import os
 from openpyxl import Workbook
 import csv
  
@@ -10,73 +11,87 @@ msgTime = []
 msgSender = []
 msg = []
  
-# TODO: make the script open any text file and/or loop over each text fil
+# Get current working directory
+cwd = os.getcwd()
 
-# Opens file and loops over each row, looking for dare, time, sender and message
-with open('whatsapp_chat.txt', 'r', encoding='utf-8') as f:
- 
-    test = f.readlines()
- 
-    start = 1
-    numItems = len(test)
- 
-    want = range(start, numItems)
- 
-    for row in want:
- 
-       # set Date Pattern
-        datePattern = '(\d+/\d+/\d+)'
- 
-        try:
-            date = re.search(datePattern, test[row]).group(0)
-        except AttributeError:
-            date = "No Date"
- 
-        msgDate.append(date)
- 
-        # Set time pattern
-        timePattern = '\d+:\d+'
- 
-        try:
-            time = re.search(timePattern, test[row]).group(0)
-        except AttributeError:
-            time = "No Time"
- 
-        msgTime.append(time)
- 
-        # Set person pattern (every value between - and :)
-        personPattern = '(?s)(?<=-\s).*?(?=:)'
- 
-        try:
-            person = re.search(personPattern, test[row]).group(0).replace("- ", "")
-        except AttributeError:
-            person = "No Person"
- 
-        msgSender.append(person)
- 
-        # set message pattern
-        messagePattern = '(:\s).*'    
- 
-        try:
-            message = re.search(messagePattern, test[row]).group(0).replace(": ", "")
-        except AttributeError:
-            message = "No message"
- 
-        msg.append(message)
- 
-# pandas dataframe created from the lists
-df = pd.DataFrame(list(zip(msgDate, msgTime, msgSender, msg)),
-                  columns=['Date', 'Time', 'Sender', 'Message'])
- 
-# datafram saved to CSV
-df.to_csv('whatsapp_chat.csv', index=False)
+# loop over all files in CWD
+for filename in os.listdir(cwd):
+    
+    # search for all text files that aren't the requirements file
+    if filename != ("requirements.txt") and filename.endswith(".txt") : 
 
-# CSV coverted to Excel format
-wb = Workbook()
-ws = wb.active
-with open('whatsapp_chat.csv', 'r', encoding='utf-8') as f:
-    for row in csv.reader(f):
-        ws.append(row)
-wb.save('whatsapp_chat.xlsx')
+        # Opens file and loops over each row, looking for dare, time, sender and message
+        with open(filename, 'r', encoding='utf-8') as f:
+        
+            test = f.readlines()
+        
+            start = 1
+            numItems = len(test)
+        
+            want = range(start, numItems)
+        
+            for row in want:
+        
+            # set Date Pattern
+                datePattern = '(\d+/\d+/\d+)'
+        
+                try:
+                    date = re.search(datePattern, test[row]).group(0)
+                except AttributeError:
+                    date = "No Date"
+        
+                msgDate.append(date)
+        
+                # Set time pattern
+                timePattern = '\d+:\d+'
+        
+                try:
+                    time = re.search(timePattern, test[row]).group(0)
+                except AttributeError:
+                    time = "No Time"
+        
+                msgTime.append(time)
+        
+                # Set person pattern (every value between - and :)
+                personPattern = '(?s)(?<=-\s).*?(?=:)'
+        
+                try:
+                    person = re.search(personPattern, test[row]).group(0).replace("- ", "")
+                except AttributeError:
+                    person = "No Person"
+        
+                msgSender.append(person)
+        
+                # set message pattern
+                messagePattern = '(:\s).*'    
+        
+                try:
+                    message = re.search(messagePattern, test[row]).group(0).replace(": ", "")
+                except AttributeError:
+                    message = "No message"
+        
+                msg.append(message)
+
+                # pandas dataframe created from the lists
+        
+        df = pd.DataFrame(list(zip(msgDate, msgTime, msgSender, msg)),
+                        columns=['Date', 'Time', 'Sender', 'Message'])
+        
+        # datafram saved to CSV
+        df.to_csv('whatsapp_chat.csv', index=False)
+
+        # CSV coverted to Excel format
+        wb = Workbook()
+        ws = wb.active
+        with open('whatsapp_chat.csv', 'r', encoding='utf-8') as f:
+            for row in csv.reader(f):
+                ws.append(row)
+        wb.save('whatsapp_chat.xlsx')
+
+    else:
+        continue
+
+
+# TODO: put filename in outputted files
 
 # TODO: create folder for each txt file, with a input/output folders
